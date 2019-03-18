58baf8465acbc27daed4fafda6231848fd820ff2
simehaa <simen.haapnes@gmail.com>
2019-03-18 13:59:50 +0100

    readfile continued

--- IN3200_PE_simehaa/pagerank.c
+++ IN3200_PE_simehaa/pagerank.c
@@ -1,5 +1,7 @@
 #include "pagerank.h"
 
 int main() {
+  char* filename = "8-webpages.txt";
+  read_graph_from_file(filename);
   return 0;
 }
--- IN3200_PE_simehaa/pagerank.h
+++ IN3200_PE_simehaa/pagerank.h
@@ -4,19 +4,73 @@
 #include <stdlib.h>
 
 void read_graph_from_file(char* filename) {
-  int nodes, edges;
-  // double *data = malloc(n * sizeof *data);
-  FILE *FilePtr;
+  FILE *fp;
   char *line = NULL;
   size_t len = 0;
-  FilePtr = fopen(filename, "r");
-  if (fp == NULL)
-    exit(EXIT_FAILURE);
+  ssize_t read;
 
-  getline(&line, &len, FilePtr);
-  getline(&line, &len, FilePtr);
+  fp = fopen(filename, "r");
+  if (fp == NULL)
+      exit(1);
+
+  int nodes, edges;
+  // Read first 4 lines, extract #nodes and #edges
+  read = getline(&line, &len, fp);
+  read = getline(&line, &len, fp);
+  read = getline(&line, &len, fp);
+  sscanf(line, "%*s %*s %i %*s %i", &nodes, &edges);
+  read = getline(&line, &len, fp);
+
+  int *FromNodeId = malloc(edges * sizeof *FromNodeId);
+  int *ToNodeId = malloc(edges * sizeof *ToNodeId);
+  int *incoming = calloc(nodes, sizeof *incoming);
+  // Extract data
+  for (int i=0; i<edges; i++) {
+    read = getline(&line, &len, fp);
+    sscanf(line, "%i %i", &FromNodeId[i], &ToNodeId[i]);
+    incoming[ToNodeId[i]]++;
+  }
+
+  // Find column values (constant along each column)
+  double *col_vals = malloc(nodes * sizeof *col_vals);
+  for (int i=0; i<nodes; i++) {
+    col_vals[i] = 1.0/incoming[i];
+    printf("%f\n", col_vals[i]);
+  }
+  free(FromNodeId);
+  free(ToNodeId);
+  free(incoming);
+  free(col_vals);
+  /*
+  double *val = malloc(edges * sizeof *val);
+  int *col = malloc(edges * sizeof *col);
+  int *row = malloc(nodes * sizeof *row);
+  free(val);
+  free(col);
+  free(row);*/
+  fclose(fp);
+  if (line)
+     free(line);
+  /*
+  int nodes, edges;
+  FILE *FilePtr;
+  FilePtr = fopen(filename, "r");
+  if (FilePtr == NULL) {
+    exit(1);
+  }
+  fclose(FilePtr);
+
+  char *line = NULL;
+  size_t len = 0;
+  ssize_t read;
+
+  read = getline(&line, &len, FilePtr);
+  printf("%ld", read);
+  read = getline(&line, &len, FilePtr);
   fscanf(FilePtr,"%i %i", &nodes, &edges);
   getline(&line, &len, FilePtr);
+  printf("%i %i \n", nodes, edges);
+  free(line);
 
   int *data = malloc(2*edges * sizeof *data); // 34 long
   int FromNodeId, ToNodeId;
@@ -28,15 +82,15 @@ void read_graph_from_file(char* filename) {
   fclose(FilePtr);
   free(line);
   free(data);
-  exit(EXIT_SUCCESS);
+  exit(EXIT_SUCCESS);*/
 }
 
-double* PageRank_iterations(double damping_const, double threshold) {
-
+/*double* PageRank_iterations(double damping_const, double threshold) {
+  return
 }
 
 void top_n_webpages(int n) {
   //
-}
+}*/
 
 #endif // PAGERANK_H
