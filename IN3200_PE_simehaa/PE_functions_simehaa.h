bbff27c6309189790ad66660d74873213c79eb58
simehaa <simen.haapnes@gmail.com>
2019-03-20 13:19:51 +0100

    CRS MVM complete, read_graph complete

--- IN3200_PE_simehaa/PE_functions_simehaa.h
+++ IN3200_PE_simehaa/PE_functions_simehaa.h
@@ -9,6 +9,8 @@ void sort_perm(int*, int*, int, int);
 void print_sparse_matrix(double*, int*, int*, int, int);
 void read_graph_from_file(char*);*/
 
+void print_sparse_matrix(double*, int*, int*, int, int);
+
 void swap(int *a, int *b) {
   int t=*a; *a=*b; *b=t;
 }
@@ -27,6 +29,165 @@ void sort_perm(int *arr, int *perm, int beg, int end) {
     sort_perm(arr, perm, r, end);
   }
 }
+
+void read_graph_from_file(char* filename, int* n, int* e, int maxEdges, int*
+    numDangling, double** val, int** col_idx, int** row_ptr, int** dangling_idx)
+{
+  FILE *fp;
+  char *line = NULL;
+  size_t len = 0;
+  ssize_t read;
+
+  fp = fopen(filename, "r");
+  if (fp == NULL)
+    exit(0);
+
+  // Read first 4 lines, extract #nodes and #edges
+  int edges, nodes;
+  read = getline(&line, &len, fp);
+  read = getline(&line, &len, fp);
+  read = getline(&line, &len, fp);
+  sscanf(line, "%*s %*s %i %*s %i", &nodes, &edges);
+  read = getline(&line, &len, fp);
+  if (maxEdges)
+    edges = maxEdges;
+
+  int *FromNodeId = malloc(edges * sizeof *FromNodeId);
+  int *ToNodeId = malloc(edges * sizeof *ToNodeId);
+  int *number_outgoing = calloc(nodes, sizeof *number_outgoing);
+  int *number_incoming = calloc(nodes, sizeof *number_incoming);
+  int *perm = malloc(edges * sizeof *perm);
+
+  // Extract data
+  for (int i=0; i<edges; i++) {
+    read = getline(&line, &len, fp);
+    sscanf(line, "%i %i", &FromNodeId[i], &ToNodeId[i]);
+    number_outgoing[FromNodeId[i]]++; // L[i]
+    number_incoming[ToNodeId[i]]++; // K[i]
+    perm[i] = i;
+  }
+
+  /* Matrix element:
+   * if K[j] != 0 and i != j,
+   *     a_ij = 1/L[j],
+   * else:
+   *     a_ij = 0
+   * Matrix will be set up as a sparse matrix according to CRS.
+   */
+
+  nodes = FromNodeId[edges-1] + 1; // According to maxedges
+  (*n) = nodes;
+  (*e) = edges;
+  fclose(fp);
+  free(line);
+
+  // Count dangling webpages
+  for (int i=0; i<nodes; i++) {
+    if (number_outgoing[i] == 0)
+      (*numDangling)++;
+  }
+
+  // save indices of dangling nodes
+  int idx = 0;
+  *dangling_idx = malloc((*numDangling) * sizeof *dangling_idx);
+  for (int i=0; i<nodes; i++) {
+    if (number_outgoing[i] == 0) {
+      (*dangling_idx)[idx] = i; idx++;
+    }
+  }
+
+  // Find arrays: col_vals, dangling_idx, row_ptr
+  double col_vals[nodes];
+  *row_ptr = malloc((nodes+1) * sizeof *row_ptr);
+  (*row_ptr)[nodes] = edges;
+  int sum = 0;
+  for (int i=0; i<nodes; i++) {
+    if (number_incoming[i]) {
+      col_vals[i] = 1.0/number_outgoing[i];
+      (*row_ptr)[i] = sum;
+    } else {
+      col_vals[i] = 0;
+    }
+    sum += number_incoming[i];
+  }
+
+  // Sort perm correctly by ToNodeId from least to greatest node
+  // Each node-chunk in ToNodeId sorted by FromNodeId
+  sort_perm(ToNodeId, perm, 0, edges);
+  int beg=0, end=0;
+  for (int node=0; node<nodes; node++) {
+    end += number_incoming[node];
+    sort_perm(FromNodeId, perm, beg, end);
+    beg = end;
+  }
+
+  // Set up arrays: val, col_idx
+  *val = malloc(edges * sizeof *val);
+  *col_idx = malloc(edges * sizeof *col_idx);
+  for (int i=0; i<edges; i++) {
+    (*col_idx)[i] = FromNodeId[perm[i]];
+    (*val)[i] = col_vals[(*col_idx)[i]];
+  }
+
+  // print_sparse_matrix(val, col_idx, row_ptr, nodes, edges);
+
+  free(FromNodeId);
+  free(ToNodeId);
+  free(number_outgoing);
+  free(number_incoming);
+  free(perm);
+}
+
+void PageRank_iterations(double dampConst, double threshold, int nodes, int edges,
+    int numDangling, double** val, int* col_idx, int* row_ptr, int* dangling_idx,
+    double* x)
+{
+  // factors and constants
+  double nth = 1.0 / (double) nodes;
+  double factor2 = dampConst*nth;
+  double factor1 = nth - factor2;
+  int iter=0, maxiter = 1;
+  int idx = 0;
+  double diff = 1.0;
+  double c; // (1 - d + d*W)/n
+  double *temp = malloc(edges * sizeof *temp);
+  // initialize x
+  for (int i=0; i<nodes; i++) x[i] = nth;
+  // update val: d * val
+  for (int i=0; i<edges; i++) (*val)[i] *= dampConst;
+
+  for (int i=0; i<nodes; i++){
+    printf("%f, ", x[i]);
+  }
+  printf("\n\n");
+
+  while (diff > threshold && iter < maxiter) {
+    double W = 0;
+    for (int i=0; i<numDangling; i++) W += x[dangling_idx[i]];
+    W *= factor2;
+    W += factor1;
+    // Matrix vector multiplication
+    idx = 0;
+    for (int i=0; i<nodes; i++) {
+      temp[i] = W;
+      for (int j=row_ptr[i]; j<row_ptr[i+1]; j++) {
+        temp[i] += (*val)[idx] * x[col_idx[j]]; idx++;
+      }
+    }
+    for (int i=0; i<nodes; i++) x[i] = temp[i];
+    iter++;
+  }
+  for (int i=0; i<nodes; i++){
+    printf("%f, ", x[i]);
+  }
+  printf("\n\n");
+}
+
+/*void top_n_webpages(int n) {
+  //
+}*/
+
+
 
 void print_sparse_matrix(double* val, int* col_idx, int* row_ptr, int nodes, int edges) {
   // Print of val array
@@ -53,113 +214,6 @@ void print_sparse_matrix(double* val, int* col_idx, int* row_ptr, int nodes, int
   }
   printf("%i]\n\n",row_ptr[nodes-1]);
 }
-
-
-void read_graph_from_file(char* filename, int maxEdges) {
-  FILE *fp;
-  char *line = NULL;
-  size_t len = 0;
-  ssize_t read;
-
-  fp = fopen(filename, "r");
-  if (fp == NULL)
-    exit(0);
-
-  int nodes, edges;
-  // Read first 4 lines, extract #nodes and #edges
-  read = getline(&line, &len, fp);
-  read = getline(&line, &len, fp);
-  read = getline(&line, &len, fp);
-  sscanf(line, "%*s %*s %i %*s %i", &nodes, &edges);
-  read = getline(&line, &len, fp);
-  edges = maxEdges;
-
-  int *FromNodeId = malloc(edges * sizeof *FromNodeId);
-  int *ToNodeId = malloc(edges * sizeof *ToNodeId);
-  int *number_outgoing = calloc(nodes, sizeof *number_outgoing);
-  int *number_incoming = calloc(nodes, sizeof *number_incoming);
-  int *perm = malloc(edges * sizeof *perm);
-
-  // Extract data
-  for (int i=0; i<edges; i++) {
-    read = getline(&line, &len, fp);
-    sscanf(line, "%i %i", &FromNodeId[i], &ToNodeId[i]);
-    number_outgoing[FromNodeId[i]]++; // L[i]
-    number_incoming[ToNodeId[i]]++; // K[i]
-    perm[i] = i;
-  }
-  /* Matrix element:
-   * if K[j] != 0 and i != j,
-   *     a_ij = 1/L[j],
-   * else:
-   *     a_ij = 0
-   * Matrix will be set up as a sparse matrix according to CRS.
-   */
-
-  nodes = FromNodeId[edges-1]; // According to maxedges
-  fclose(fp);
-  free(line);
-
-  // Count dangling webpages
-  double col_vals[edges];
-  int num_dangling = 0;
-  for (int i=0; i<nodes; i++) {
-    if (number_outgoing[i] == 0)
-      num_dangling++;
-  }
-
-  // Find arrays: col_vals, dangling_idx, row_ptr
-  int *row_ptr = malloc(nodes * sizeof *row_ptr);
-  int dangling_idx[num_dangling]; num_dangling = 0;
-  int sum = 0;
-  for (int i=0; i<nodes; i++) {
-    if (number_outgoing[i] != 0) {
-      col_vals[i] = 1.0/number_outgoing[i];
-      row_ptr[i] = sum;
-    } else {
-      dangling_idx[num_dangling] = i; num_dangling++;
-    }
-    sum += number_incoming[i];
-  }
-
-  // Sort perm correctly by ToNodeId from least to greatest node
-  // Each node-chunk in ToNodeId sorted by FromNodeId
-  sort_perm(ToNodeId, perm, 0, edges);
-  int beg=0, end=0;
-  for (int node=0; node<nodes; node++) {
-    end += number_incoming[node];
-    sort_perm(FromNodeId, perm, beg, end);
-    beg = end;
-  }
-
-  // Set up arrays: val, col_idx
-  double *val = malloc(edges * sizeof *val);
-  int *col_idx = malloc(edges * sizeof *col_idx);
-  int elems, idx=0;
-  for (int i=0; i<edges; i++) {
-    col_idx[i] = FromNodeId[perm[i]];
-    val[i] = col_vals[col_idx[i]];
-  }
-
-  // print_sparse_matrix(val, col_idx, row_ptr, nodes, edges);
-
-  free(FromNodeId);
-  free(ToNodeId);
-  free(number_outgoing);
-  free(number_incoming);
-  free(val);
-  free(col_idx);
-  free(row_ptr);
-  free(perm);
-}
-
-/*double* PageRank_iterations(double damping_const, double threshold) {
-  return
-}
-
-void top_n_webpages(int n) {
-  //
-}*/
 
 
 #endif // PE_FUNCTIONS_SIMEHAA_H
--- IN3200_PE_simehaa/PE_main_simehaa.c
+++ IN3200_PE_simehaa/PE_main_simehaa.c
@@ -6,22 +6,44 @@
 int main(int argc, char **argv) {
   if (argc < 5) {
     printf("Usage error:\n");
-    printf("$ ./pagerank.exe filename dampConst convThreshold nTopWebpages\n");
+    printf("$ ./pagerank.exe filename dampConst threshold nTopWebpages\n");
     exit(0);
   }
   char* filename = argv[1];
-  double dampConst = atoi(argv[2]);
-  double convThreshold = atoi(argv[3]);
+  double dampConst = atof(argv[2]);
+  double threshold = atof(argv[3]);
   int nTopWebpages = atoi(argv[4]);
+  int n = 0;
+  int e = 0;
+  int maxEdges = 0; // unspecified if 0
+  int numDangling = 0;
+  double* val = NULL;
+  int* col_idx = NULL;
+  int* row_ptr = NULL;
+  int* dangling_idx = NULL;
 
   struct timespec start, end;
 
   clock_gettime(CLOCK_REALTIME, &start);
-  read_graph_from_file(filename, 900000); // Segmentation fault for MaxEdges > 927 000.
+  read_graph_from_file(filename, &n, &e, maxEdges, &numDangling, &val, &col_idx,
+    &row_ptr, &dangling_idx);
   clock_gettime(CLOCK_REALTIME, &end);
-
   double accum = (end.tv_sec-start.tv_sec) + 1e-9*(end.tv_nsec-start.tv_nsec);
   printf("read_graph_from_file, time = %lf s\n", accum);
 
+  double *x = malloc(n * sizeof *x);
+
+  clock_gettime(CLOCK_REALTIME, &start);
+  PageRank_iterations(dampConst, threshold, n, e, numDangling, &val, col_idx,
+    row_ptr, dangling_idx, x);
+  clock_gettime(CLOCK_REALTIME, &end);
+  accum = (end.tv_sec-start.tv_sec) + 1e-9*(end.tv_nsec-start.tv_nsec);
+  printf("PageRank_iterations, time = %lf s\n", accum);
+
+  free(val);
+  free(col_idx);
+  free(row_ptr);
+  free(dangling_idx);
+  free(x);
   return 0;
 }
